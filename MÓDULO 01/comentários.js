1de227c41f245f9cac142a0f6021ed8fbb4eb0ea
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-05 05:50:52 -0400

    Curso de Algoritmos e Lógica com JS

---
+++ MÓDULO 01/comentários.js
@@ -0,0 +1,8 @@
+// Nesta aula estou aprendendo a utilizar comentários em JavaScript.
+console.log("Kalleb Carvalho");
+console.log("Brasil");
+console.log("samba");
+
+/* Esse tipo de comentário é possível fazer quebrando linhas utilizando a barra e o asterisco, 
+e finalizando com o asterisco e a barra.
+ */
