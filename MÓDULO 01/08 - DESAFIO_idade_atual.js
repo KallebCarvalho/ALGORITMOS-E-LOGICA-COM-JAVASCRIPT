5f1610c1b120366d681c35ff99672688f418c37f
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-05 21:18:51 -0400

    Adicionando os desafios de aprendizagem

---
+++ MÓDULO 01/08 - DESAFIO_idade_atual.js
@@ -0,0 +1,14 @@
+//Neste desafio a ideia é calcular a idade atual de uma pessoa,
+// A partir do ano de nascimento e do ano atual.
+
+var anoNascimento = 1997; // Number
+var anoAtual = 2026; // Number
+
+var idadeAtual = anoAtual - anoNascimento; // Number
+console.log("A minha idade atual é:" + idadeAtual + " anos"); 
+// Concatenando a string com a variável idadeAtual
+// Ou fazer uso de template string, que é uma forma mais moderna de concatenar strings com variáveis.
+//exemplo de template string:
+console.log(`A minha idade atual é: ${idadeAtual} anos`); // Usar crases invertidas (``) 
+// para utilizar template string
+// Concatenando a string com a variável idadeAtual usando template string
---
+++ MÓDULO 01/09 - DESAFIO_horas.js
@@ -0,0 +1,7 @@
+//Neste desafio vamos fazer a conversão de horas para minutos e segundos, a partir de um valor em horas.
+//utilizando operadores aritméticos.
+
+var horas = 1; // Number
+var minutos = horas * 60; // Number
+var segundos = horas * 3600; // Number
+console.log(`${horas} horas equivalem a ${minutos} minutos e ${segundos} segundos.`);
