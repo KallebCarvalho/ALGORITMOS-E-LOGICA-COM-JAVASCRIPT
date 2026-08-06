b29507fd912b13bb35044d73e20f79850d53480e
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-06 09:54:48 -0400

    Comparador igual ou maior que

---
+++ FUNDAMENTOS/OPERADORES DE COMPARAÇÃO/03 - MAIOR_OU_IGUAL.js
@@ -0,0 +1,19 @@
+//Apredendo sobre operadores de comparação
+
+//Maior que (>) - Compara se o valor da esquerda é maior que o valor da direita.
+
+var numero17 = 100; // Number
+var numero18 = 200; // Number
+console.log(numero17 > numero18); 
+// false - O valor da esquerda é menor que o valor da direita (100 < 200)
+
+var numero19 = 200; // Number
+var numero20 = 50; // Number
+console.log(numero19 > numero20);
+// true - O valor da esquerda é maior que o valor da direita (200 > 50)
+
+// Maior ou igual (>=) - Compara se o valor da esquerda é maior ou igual ao valor da direita.
+
+var numero21 = 100; // Number
+var numero22 = 100; // Number
+console.log(numero21 >= numero22);
