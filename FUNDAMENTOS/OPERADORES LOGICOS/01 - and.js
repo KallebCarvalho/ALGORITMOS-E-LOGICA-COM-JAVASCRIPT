d0e7abaea507cc1d0a50d19e3aaeb4d52c6220a3
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-07 20:52:32 -0400

    Alterando

---
+++ FUNDAMENTOS/OPERADORES LOGICOS/01 - and.js
@@ -0,0 +1,10 @@
+//Operador lógico AND (&&)
+//Retorna true se ambos os valores forem true, caso contrário retorna false
+
+const valor1 = true;
+const valor2 = false;
+console.log(valor1 && valor2);// falso - Um dos valores é false, então o resultado é false
+
+const valor3 = true;
+const valor4 = true;
+console.log(valor3 && valor4);// verdadeiro - Ambos os valores são true, então o resultado é true
---
+++ FUNDAMENTOS/OPERADORES LOGICOS/02 - ou_or.js
@@ -0,0 +1,11 @@
+//Operador logico OU OR (||)
+//Retorna true se pelo menos um dos valores for true, caso contrário retorna false
+
+var valor1 = true;
+var valor2 = false;
+console.log(valor1 || valor2);// verdadeiro - Um dos valores é true, então o resultado é true
+
+var valor3 = false;
+var valor4 = false;
+console.log(valor3 || valor4); // Falso - ambos os valores são false, então o resultado é false.
+
