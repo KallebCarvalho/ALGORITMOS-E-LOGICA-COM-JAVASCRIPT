63d6ba946fc76ae2a8597e88644932562d3960d8
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-07 21:25:10 -0400

    condicionais

---
+++ FUNDAMENTOS/ESTRUTURA DE CONTROLE/Condicional.js
@@ -0,0 +1,17 @@
+// Condicional "SE" (IF) 
+// Estrutura de controle que permite executar um bloco de código se uma condição for verdadeira
+
+//Se (if) a idade for maior ou igual a 18
+// então execute o bloco de código dentro das chaves
+//BLOCO DE CÓDIGO é tudo que está dentro das chaves {}
+
+const idade = 15;
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+}
+//Se (if) a idade for menor que 18
+// então execute o bloco de código dentro das chaves
+const idade2 = 16;
+if (idade2 < 18) {
+    console.log("Você é menor de idade.");
+}
---
+++ FUNDAMENTOS/ESTRUTURA DE CONTROLE/Else.js
@@ -0,0 +1,4 @@
+//Condicional Else (Senão)
+//Else sempre tem que estar ligado a um if, ou seja, não pode existir um else sozinho, 
+// ele sempre vai depender de um if para ser executado.
+
