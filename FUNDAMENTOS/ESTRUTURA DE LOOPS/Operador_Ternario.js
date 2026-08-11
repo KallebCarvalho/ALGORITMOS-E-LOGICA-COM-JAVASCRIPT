70f083b77957200b235f829d08f1920b57f08070
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-10 21:16:47 -0400

    Operador ternario

--- FUNDAMENTOS/ESTRUTURA DE LOOPS/Operador_Ternario.js
+++ FUNDAMENTOS/ESTRUTURA DE LOOPS/Operador_Ternario.js
@@ -4,3 +4,9 @@
 let media = 8;
 media >= 8 ? console.log("Aluno aprovado") : console.log("Aluno reprovado");
 
+//Explicação:
+//Se a media do aluno for maior ou igual a 8, 
+// o código dentro do ? : é executado, caso contrário, o código dentro do : é executado.
+//Logo o aluno aprovado é exibido.
+//caso a media do aluno fosse menor que 8
+//O proximo código será executado, logo o aluno reprovado é exibido.
