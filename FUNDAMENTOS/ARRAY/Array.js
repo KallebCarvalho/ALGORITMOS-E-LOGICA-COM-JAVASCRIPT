060e1e180a44d80497674c2628d1336cf688fe4d
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-12 20:46:33 -0400

    Manipulando Array

---
+++ FUNDAMENTOS/ARRAY/Array.js
@@ -0,0 +1,16 @@
+//Array é uma coleção de valores, onde se pode armazenar qualquer tipo de dado.
+//Array são coleção de dados.
+
+
+
+//index          0   1   2   3   4   5   6   7   8   9
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//Array é caracterizado por colchetes.
+//Index sempre começa em 0.
+
+console.log(numeros.length);
+//Length é o número de elementos que o array tem.
+console.log(numeros[0]);//0 é o primeiro elemento do array.
+console.log(numeros[1]);// 1 é o segundo elemento do array.
+console.log(numeros[2]);// 2 é o terceiro elemento do array.
