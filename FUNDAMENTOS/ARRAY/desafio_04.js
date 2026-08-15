620cc2360cb9aee3b3fde434c14af832d4d6dc57
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-15 11:17:33 -0400

    Numeros multiplos

--- FUNDAMENTOS/ARRAY/desafio_04.js
+++ FUNDAMENTOS/ARRAY/desafio_04.js
@@ -15,9 +15,11 @@ let numerosMultiplosde3 = [];
 
 numeros.forEach((numeros) => { 
     if (numeros % 3 === 0){
+    //Se o numero for multiplo de 3 e estritamente igual a 0 então ele é multiplo de 3.
         numerosMultiplosde3.push(numeros);
+        //Push coloco um elemento dentro de um array.
       
     }
 
 })
-  console.log(numerosMultiplosde3);
+  console.log(`Lista de números que são multiplos de 3: ${numerosMultiplosde3}`);
