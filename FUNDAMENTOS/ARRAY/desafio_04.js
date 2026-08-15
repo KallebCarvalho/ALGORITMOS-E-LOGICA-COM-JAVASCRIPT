84941505d44de6077bd5b060791a636d9a9eef9b
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-15 11:15:35 -0400

    Multiplos

---
+++ FUNDAMENTOS/ARRAY/desafio_04.js
@@ -0,0 +1,23 @@
+// DESAFIO 04
+// Crie um array com esses numeros: [6, 12, 5, 9, 14, 27]
+// Você deve devolver um array com todos os numeros que são multiplos de 3
+
+
+// Passo 01 - Criar um array com os números
+
+const numeros = [6, 12, 5, 9, 14, 27];
+console.log(numeros);
+
+// Passo 02 - Criar um array vazio
+let numerosMultiplosde3 = [];
+
+// Passo 03 - Percorrer cada elemento do array e verificar se ele é multiplo de 3
+
+numeros.forEach((numeros) => { 
+    if (numeros % 3 === 0){
+        numerosMultiplosde3.push(numeros);
+      
+    }
+
+})
+  console.log(numerosMultiplosde3);
