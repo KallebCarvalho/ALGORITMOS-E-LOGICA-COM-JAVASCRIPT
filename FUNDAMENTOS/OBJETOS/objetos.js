b4b5cc146718b2056038f248b8578011cbe57000
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-16 21:31:28 -0400

    Objetos

--- FUNDAMENTOS/OBJETOS/objetos.js
+++ FUNDAMENTOS/OBJETOS/objetos.js
@@ -11,3 +11,8 @@ const aluno ={
 }
 //Objetos são construidos usando chaves e valores
 console.log(aluno);
+console.log(aluno.nome);
+console.log(aluno.idade);
+console.log(aluno.salario);
+console.log(aluno.carreira);
+
