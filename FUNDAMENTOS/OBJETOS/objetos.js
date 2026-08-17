b6eb89724ee067308b4868d3a4fb86d125c8d7b5
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-16 21:29:52 -0400

    Aprendendo sobre objetos

---
+++ FUNDAMENTOS/OBJETOS/objetos.js
@@ -0,0 +1,13 @@
+//OBJETOS
+//Aprendendo sobre objetos 
+
+
+//Objetos
+const aluno ={
+    nome: "João",
+    idade: 20,
+    salario: 1000,
+    carreira: "Engenheiro"
+}
+//Objetos são construidos usando chaves e valores
+console.log(aluno);
