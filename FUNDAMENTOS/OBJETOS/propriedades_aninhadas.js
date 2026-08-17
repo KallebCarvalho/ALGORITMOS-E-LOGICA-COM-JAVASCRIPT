e80f18d45bbbbbeb7ad4edee83126e6eb50c2788
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-16 21:55:49 -0400

    aninhadas

---
+++ FUNDAMENTOS/OBJETOS/propriedades_aninhadas.js
@@ -0,0 +1,18 @@
+//Propriedades aninhadas
+//O QUE SÃO PROPRIEDADES ANINHADAS
+//Propriedades aninhadas são propriedades que estão dentro de outra propriedade.
+
+
+const pessoa = {
+    nome: "João",
+    idade: 20,
+    salario: 1000,
+    carreira: "Engenheiro",
+    endereco: {
+        rua: "Rua do Campo Grande",
+        numero: 123,
+        bairro: "Centro",
+        cidade: "Campinas",
+        estado: "SP"
+    }
+}
